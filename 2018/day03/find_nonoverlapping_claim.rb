502e09f14c3b768d490893bfb0dae47fe57c6eb2
Stephen Jennings <stephen.g.jennings@gmail.com>
2018-12-03 22:33:26 -0800

    2018: day 3 (cleanup)

--- 2018/day03/find_nonoverlapping_claim.rb
+++ 2018/day03/find_nonoverlapping_claim.rb
@@ -1,4 +1,3 @@
-
 claims = Array.new
 canvas = Array.new(1000) {Array.new(1000, 0)}
 
@@ -6,9 +5,9 @@ canvas = Array.new(1000) {Array.new(1000, 0)}
 file = File.new("input.txt")
 file.readlines.each do |line|
   parts = line.split(" ")
-  claim_id = parts[0][1..(parts[0].length)]
-  coords = parts[2][0..-2].split(",").map {|x| Integer(x)}
-  size = parts[3].split("x").map {|x| Integer(x)}
+  claim_id = parts[0][1...(parts[0].length)]
+  coords = parts[2][0...-1].split(",").map &:to_i
+  size = parts[3].split("x").map &:to_i
   claim = {
     claim_id: claim_id,
     coords: coords,
@@ -21,8 +20,10 @@ end
 claims.each do |claim|
   coords = claim[:coords]
   size = claim[:size]
-  (coords[0]...(coords[0]+size[0])).each do |x|
-    (coords[1]...(coords[1]+size[1])).each do |y|
+  xs = coords[0]...(coords[0] + size[0])
+  ys = coords[1]...(coords[1] + size[1])
+  xs.each do |x|
+    ys.each do |y|
       canvas[x][y] = canvas[x][y] + 1
     end
   end
@@ -34,8 +35,10 @@ nonoverlapped = claims.each do |claim|
   coords = claim[:coords]
   size = claim[:size]
 
-  break claim[:claim_id] if (coords[0]...(coords[0]+size[0])).all? do |x|
-    (coords[1]...(coords[1]+size[1])).all? do |y|
+  xs = coords[0]...(coords[0] + size[0])
+  ys = coords[1]...(coords[1] + size[1])
+  break claim[:claim_id] if xs.all? do |x|
+    ys.all? do |y|
       canvas[x][y] == 1
     end
   end
--- 2018/day03/find_overlapping_squares.rb
+++ 2018/day03/find_overlapping_squares.rb
@@ -3,12 +3,14 @@ canvas = Array.new(1000) {Array.new(1000, 0)}
 file = File.new("input.txt")
 file.readlines.each do |line|
   parts = line.split(" ")
-  claim_id = parts[0][1..(parts[0].length)]
-  coords = parts[2][0..-2].split(",").map {|x| Integer(x)}
+  claim_id = parts[0][1...(parts[0].length)]
+  coords = parts[2][0...-1].split(",").map &:to_i
 
-  size = parts[3].split("x").map {|x| Integer(x)}
-  (coords[0]...(coords[0]+size[0])).each do |x|
-    (coords[1]...(coords[1]+size[1])).each do |y|
+  size = parts[3].split("x").map &:to_i
+  xs = coords[0]...(coords[0] + size[0])
+  ys = coords[1]...(coords[1] + size[1])
+  xs.each do |x|
+    ys.each do |y|
       canvas[x][y] = canvas[x][y] + 1
     end
   end
